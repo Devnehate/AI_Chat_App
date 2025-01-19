b3b8b74daacff2e9169a8aaa9cca2ae6526174ec
Dev Nehate <ndev7567@gmail.com>
2025-01-19 14:51:10 +0530

    first commit

---
+++ backend/db/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+function connect(){
+    mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+        console.log("DB connected");
+        })
+        .catch((err) => {
+        console.log("DB connection error: ", err);
+    });
+}
+
+export default connect;
