6693ef00af26655583d7a011e4bc0040b055ba76
Dev Nehate <ndev7567@gmail.com>
2025-01-20 20:56:11 +0530

    add project model, routes, and controller for project creation

---
+++ backend/routes/project.routes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import {body} from 'express-validator';
+import * as projectController from '../controllers/project.controller.js';
+import * as  authMiddleware from '../middleware/auth.middleware.js';
+
+const router = Router();
+
+
+router.post('/create',
+    body('name').isString().withMessage('Name is required'),
+    authMiddleware.authUser,
+    projectController.createProject
+)
+
+export default router;
