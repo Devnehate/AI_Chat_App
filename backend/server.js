0d6cb754513548f3e6f0d5267b08cdfdae3ebed2
Dev Nehate <ndev7567@gmail.com>
2025-01-21 20:27:03 +0530

    add socket.io integration and update package dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 import 'dotenv/config';
 import http from 'http';
 import app from './app.js';
+import { Server } from 'socket.io';
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-server.listen(port, () => { 
+const io = new Server(server);
+
+io.on('connection', socket => {
+
+    console.log('a user connected');
+    socket.on('event', data => { /* … */ });
+    socket.on('disconnect', () => { /* … */ });
+});
+
+
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
