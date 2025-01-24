60b0b06c1bd7f4cd1b9f8e5f9c9a866209929d8d
Dev Nehate <ndev7567@gmail.com>
2025-01-24 13:27:55 +0530

    enhance AI service; improve response structure and add example prompts for better content generation

--- backend/services/ai.service.js
+++ backend/services/ai.service.js
@@ -3,14 +3,80 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_KEY);
 const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
-    systemInstruction:`You are an expert in MERN and Development. You have an experiance of 10 years in the development. You always write code in modular and break the code in the possible way and follw bwst practices, You use understandable comments in the code, ypu create files as needed, you wtite code while maintaining the working of previous code. You always follow the best practices of the development you never miss the edge cases and always write the code that is scalable and maintainable, In your code you always handle the errors and exceptions.`
+    generationConfig: {
+        responseMimeType: "application/json",
+
+    },
+    systemInstruction: `You are an expert in MERN and Development. You have an experiance of 10 years in the development. You always write code in modular and break the code in the possible way and follw bwst practices, You use understandable comments in the code, ypu create files as needed, you wtite code while maintaining the working of previous code. You always follow the best practices of the development you never miss the edge cases and always write the code that is scalable and maintainable, In your code you always handle the errors and exceptions.
+    
+    Examples:
+
+    <example>
+    
+    user: Create an express application
+    response:{
+    
+    "text":"this is your filetree structure of the express server".
+    "fileTree":{
+    "app.js":{
+    content:"
+    const express = require('express');
+
+    const app = express();
+
+    app.get('/', (req, res) => {
+        res.send('Hello World');
+    });
+        
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+        "
+    },
+
+    "package.json":{
+    content:"
+                {
+            "name": "temp_server",
+            "version": "1.0.0",
+            "main": "index.js",
+            "scripts": {
+                "test": "echo \"Error: no test specified\" && exit 1"
+            },
+            "keywords": [],
+            "author": "",
+            "license": "ISC",
+            "description": "",
+            "dependencies": {
+                "express": "^4.21.2"
+            }
+            }
+        "
+
+    }
+    }
+    }
+
+    </example>
+
+    <example>
+    
+    user:Hello
+    response:{
+    "text":"Hello, How can I help you today?"
+    }
+    
+    </example>
+
+    `
+
 });
 
 
 export const generateResult = async (prompt) => {
 
     const result = await model.generateContent(prompt);
-    
+
     return result.response.text()
 
 }
